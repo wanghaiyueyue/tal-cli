d7ff65cd6a8cca37fcb56adbba713efc0a8a958c
wanghaipeng <v_wanghaipeng@tal.com>
2021-04-22 18:35:18 +0800

    feat: 初始化

--- bin/tal.js
+++ bin/tal.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
 let program = require('commander')
-// 如果希望程序响应-v选项而不是-V
 program
   .version('0.0.1','-v','--version')
-  .description('项目快速初始化模板工程cli')
-  .command('init <name>', 'create project')
-  .command('add <name>','add somePlugins')
+
+program
+  .command('init <app-name>')
+  .description('Initialize a new project powered by tal-cli')
+  .action(async(name, options) => {
+    require('../lib/create')(name, options)
+  })
 
 program.parse(process.argv)
